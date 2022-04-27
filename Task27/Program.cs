28e175f121f9df91af7580cb5c28e005c8071592
ashvarvar <ashvarvar@gmail.com>
2022-04-27 22:47:20 +0300

    done

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.Write("Введите число :   ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumDigit = 0;
+
+while (number > 0)
+        {
+            int digit = number % 10;
+            sumDigit = sumDigit + digit;
+            number = number / 10;
+        }
+System.Console.Write($"Сумма цифр числа равна {sumDigit} ");
---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+      
+Console.Write("Введите размер массива :   ");
+int SizeArray = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[SizeArray];
+    
+    for (int i=0; i<SizeArray; i++)
+        {
+            Console.Write("Введите число :   ");
+            myArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+    for (int i = 0; i < SizeArray; i++)
+        {
+            Console.Write($"{ myArray[i]} ");
+        }
